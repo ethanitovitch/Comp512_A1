aa6c309f2c7da7c3c2a5e62032451a3c6ba14e04
ethanitovitch <ethan.itovitch@mail.mcgill.ca>
2021-09-22 12:53:50 -0400

    RMI stuff

---
+++ Server/Server/Middleware/ResourceMiddleware.java
@@ -0,0 +1,142 @@
+package Server.Middleware;
+
+import Server.Common.RMHashMap;
+import Server.Common.ResourceManager;
+import Server.Interface.IResourceManager;
+
+import java.rmi.RemoteException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Vector;
+
+public class ResourceMiddleware implements IResourceManager {
+
+    private static final String FLIGHTS = "Flights";
+    private static final String CARS = "Cars";
+    private static final String ROOMS = "Rooms";
+
+    protected String m_name = "";
+    protected Map<String, IResourceManager> resourceManagers = new HashMap<>();
+
+    public ResourceMiddleware(String p_name)
+    {
+        m_name = p_name;
+    }
+
+    @Override
+    public boolean addFlight(int id, int flightNum, int flightSeats, int flightPrice) throws RemoteException {
+        IResourceManager resourceManager = resourceManagers.get(FLIGHTS);
+        return resourceManager.addFlight(id, flightNum, flightSeats, flightPrice);
+    }
+
+    @Override
+    public boolean addCars(int id, String location, int numCars, int price) throws RemoteException {
+        IResourceManager resourceManager = resourceManagers.get(CARS);
+        return resourceManager.addCars(id, location, numCars, price);
+    }
+
+    @Override
+    public boolean addRooms(int id, String location, int numRooms, int price) throws RemoteException {
+        IResourceManager resourceManager = resourceManagers.get(ROOMS);
+        return resourceManager.addRooms(id, location, numRooms, price);
+    }
+
+    @Override
+    public int newCustomer(int id) throws RemoteException {
+        return 0;
+    }
+
+    @Override
+    public boolean newCustomer(int id, int cid) throws RemoteException {
+        return false;
+    }
+
+    @Override
+    public boolean deleteFlight(int id, int flightNum) throws RemoteException {
+        IResourceManager resourceManager = resourceManagers.get(FLIGHTS);
+        return resourceManager.deleteFlight(id, flightNum);
+    }
+
+    @Override
+    public boolean deleteCars(int id, String location) throws RemoteException {
+        IResourceManager resourceManager = resourceManagers.get(CARS);
+        return resourceManager.deleteCars(id, location);
+    }
+
+    @Override
+    public boolean deleteRooms(int id, String location) throws RemoteException {
+        IResourceManager resourceManager = resourceManagers.get(ROOMS);
+        return resourceManager.deleteRooms(id, location);
+    }
+
+    @Override
+    public boolean deleteCustomer(int id, int customerID) throws RemoteException {
+        return false;
+    }
+
+    @Override
+    public int queryFlight(int id, int flightNumber) throws RemoteException {
+        IResourceManager resourceManager = resourceManagers.get(FLIGHTS);
+        return resourceManager.queryFlight(id, flightNumber);
+    }
+
+    @Override
+    public int queryCars(int id, String location) throws RemoteException {
+        IResourceManager resourceManager = resourceManagers.get(CARS);
+        return resourceManager.queryCars(id, location);
+    }
+
+    @Override
+    public int queryRooms(int id, String location) throws RemoteException {
+        IResourceManager resourceManager = resourceManagers.get(ROOMS);
+        return resourceManager.queryRooms(id, location);
+    }
+
+    @Override
+    public String queryCustomerInfo(int id, int customerID) throws RemoteException {
+        return null;
+    }
+
+    @Override
+    public int queryFlightPrice(int id, int flightNumber) throws RemoteException {
+        IResourceManager resourceManager = resourceManagers.get(FLIGHTS);
+        return resourceManager.queryFlightPrice(id, flightNumber);
+    }
+
+    @Override
+    public int queryCarsPrice(int id, String location) throws RemoteException {
+        IResourceManager resourceManager = resourceManagers.get(CARS);
+        return resourceManager.queryCarsPrice(id, location);
+    }
+
+    @Override
+    public int queryRoomsPrice(int id, String location) throws RemoteException {
+        IResourceManager resourceManager = resourceManagers.get(ROOMS);
+        return resourceManager.queryRoomsPrice(id, location);
+    }
+
+    @Override
+    public boolean reserveFlight(int id, int customerID, int flightNumber) throws RemoteException {
+        return false;
+    }
+
+    @Override
+    public boolean reserveCar(int id, int customerID, String location) throws RemoteException {
+        return false;
+    }
+
+    @Override
+    public boolean reserveRoom(int id, int customerID, String location) throws RemoteException {
+        return false;
+    }
+
+    @Override
+    public boolean bundle(int id, int customerID, Vector<String> flightNumbers, String location, boolean car, boolean room) throws RemoteException {
+        return false;
+    }
+
+    @Override
+    public String getName() throws RemoteException {
+        return m_name;
+    }
+}
---
+++ Server/Server/RMI/RMIMiddleware.java
@@ -0,0 +1,102 @@
+package Server.RMI;
+
+import Server.Interface.*;
+import Server.Middleware.ResourceMiddleware;
+
+import java.rmi.NotBoundException;
+import java.util.*;
+
+import java.rmi.registry.Registry;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+public class RMIMiddleware extends ResourceMiddleware {
+	private static int s_serverPort = 1099;
+  	private static String s_serverName = "Server";
+  	private static String s_rmiPrefix = "group_23_";
+  	private static String[] serverNames = {"Flights", "Cars", "Rooms"};
+
+  	public static void main(String args[]) {
+		// Create the RMI server entry
+		try {
+			// Create a new Server object
+			RMIMiddleware server = new RMIMiddleware(s_serverName);
+
+			// Connect server to resource managers
+			for (int i = 0; i < 3; i++) {
+				server.connectResourceManager(args[i], serverNames[i]);
+			}
+
+			// Dynamically generate the stub (client proxy)
+			IResourceManager resourceManager = (IResourceManager)UnicastRemoteObject.exportObject(server, 0);
+
+			// Bind the remote object's stub in the registry
+			Registry l_registry;
+			try {
+				l_registry = LocateRegistry.createRegistry(1099);
+			} catch (RemoteException e) {
+				l_registry = LocateRegistry.getRegistry(1099);
+			}
+			final Registry registry = l_registry;
+			registry.rebind(s_rmiPrefix + s_serverName, resourceManager);
+
+			Runtime.getRuntime().addShutdownHook(new Thread() {
+				public void run() {
+					try {
+						registry.unbind(s_rmiPrefix + s_serverName);
+						System.out.println("'" + s_serverName + "' resource manager unbound");
+					}
+					catch(Exception e) {
+						System.err.println((char)27 + "[31;1mServer exception: " + (char)27 + "[0mUncaught exception");
+						e.printStackTrace();
+					}
+				}
+			});
+			System.out.println("'" + s_serverName + "' resource manager server ready and bound to '" + s_rmiPrefix + s_serverName + "'");
+		}
+		catch (Exception e) {
+			System.err.println((char)27 + "[31;1mServer exception: " + (char)27 + "[0mUncaught exception");
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		// Create and install a security manager
+		if (System.getSecurityManager() == null)
+		{
+			System.setSecurityManager(new SecurityManager());
+		}
+	}
+
+	public RMIMiddleware(String name)
+	{
+		super(name);
+	}
+
+	public void connectResourceManager(String machine, String serverName) {
+		try {
+			boolean first = true;
+			while (true) {
+				try {
+					Registry registry = LocateRegistry.getRegistry(machine, s_serverPort);
+					IResourceManager m_resourceManager = (IResourceManager)registry.lookup(s_rmiPrefix + serverName);
+					resourceManagers.put(serverName, m_resourceManager);
+					System.out.println("Connected to '" + serverName + "' server [" + machine + ":" + s_serverPort + "/" + s_rmiPrefix + serverName + "]");
+					break;
+				}
+				catch (NotBoundException|RemoteException e) {
+					if (first) {
+						System.out.println("Error connecting to '" + serverName + "' server [" + machine + ":" + s_serverPort + "/" + s_rmiPrefix + serverName + "]");
+						first = false;
+					}
+				}
+				Thread.sleep(500);
+			}
+		}
+		catch (Exception e) {
+			System.err.println((char)27 + "[31;1mServer exception: " + (char)27 + "[0mUncaught exception");
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+}
